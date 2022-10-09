77188672d89c3f02784dffb655e3e361cecb301e
Amanda Brunelli <am_sbr@outlook.com>
2022-10-09 18:37:51 -0300

    Add exercises

---
+++ exercicio-1.sql
@@ -0,0 +1,4 @@
+-- A equipe de marketing precisa fazer uma pesquisa sobre nome mais comuns de seus clientes e precisa também do nome de todos os clientes cadastrados no sistema.
+
+SELECT firstName, lastName
+FROM person.Person
