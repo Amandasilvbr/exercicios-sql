e6b3ef25618a73304dc16ee79c3e098b4f23c257
Amanda <100382936+Amandasilvbr@users.noreply.github.com>
2022-11-14 12:50:28 -0300

    Add exercises

---
+++ exercicios/exercicio-10.sql
@@ -0,0 +1,21 @@
+SELECT *
+FROM Person.Person
+WHERE FirstName LIKE 'Ovi%';
+
+--
+
+SELECT *
+FROM Person.Person
+WHERE LastName LIKE '%to';
+
+--
+
+SELECT *
+FROM Person.Person
+WHERE LastName LIKE '%essa%';
+
+--
+
+SELECT *
+FROM Person.Person
+WHERE LastName LIKE '%ro_'
