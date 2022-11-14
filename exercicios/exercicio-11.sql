aee5c7b298cdd7eb66eca1139c04c62bb8e23a36
Amanda <100382936+Amandasilvbr@users.noreply.github.com>
2022-11-14 13:17:01 -0300

    Update exercicio-11.sql

--- exercicios/exercicio-11.sql
+++ exercicios/exercicio-11.sql
@@ -12,3 +12,10 @@ WHERE FirstName LIKE 'P%';
 
 SELECT COUNT(DISTINCT(City))
 FROM Person.Address
+
+--
+
+SELECT COUNT(*)
+FROM Production.Product
+WHERE Color='RED'
+AND ListPrice BETWEEN 500 AND 1000
